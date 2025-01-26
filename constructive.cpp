18fdfc06515fcdbdac14cdbeceee06075e911ed5
jishusardar <jishusardar01@gmail.com>
2025-01-26 19:40:06 +0530

    cons

---
+++ constructive.cpp
@@ -0,0 +1,22 @@
+//bit++
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int x=0;
+//     int t;
+//     cin>>t;
+//     string s;
+//     while(t){
+//         cin>>s;
+//         if(s=="++x"||s=="x++"){
+//             x++;
+//         }
+//         else{
+//             x--;
+//         }
+//         t--;
+//     }
+//     cout<<x;
+//     return 0;
+// }
