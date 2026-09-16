078f5aeff7dbffe88214ecacbc5ad128c47e845a
Jishu Sardar <jishusardar01@gmail.com>
2026-09-17 01:33:02 +0530

    Add Ambitious Kid [A] from Codeforces

---
+++ 1866/A - Ambitious Kid/solution.cpp
@@ -0,0 +1,19 @@
+//  A. Ambitious Kid
+ 
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+    long long N;
+    cin>>N;
+    vector<long long >A(N);
+    for (long long i=0;i<N;i++)
+        cin>>A[i];
+    long long ans=INT_MAX;
+    for (long long i=0;i<N;i++) {
+        ans=min(ans,abs(0-A[i]));
+    }
+    cout<<ans<<" ";
+    return 0;
+}
