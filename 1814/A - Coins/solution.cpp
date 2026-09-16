d5cc8d37b0ef115a1aba4773bf1b6418901681c6
Jishu Sardar <jishusardar01@gmail.com>
2026-09-17 01:33:53 +0530

    Add Coins [A] from Codeforces

---
+++ 1814/A - Coins/solution.cpp
@@ -0,0 +1,20 @@
+//  A. Coins
+ 
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    long long int t;
+    cin>>t;
+    while (t--) {
+        long long int n,k;
+        cin>>n>>k;
+        if (!(n%2))
+            cout<<"YES"<<endl;
+        else if ((n-k)%2==0) {
+            cout<<"YES"<<endl;
+        }
+        else
+            cout<<"NO"<<endl;
+    }
+    return 0;
+}
