d6b1cff2c316ff10f78873f501524c6b470e368a
Jishu Sardar <jishusardar01@gmail.com>
2026-09-17 01:33:48 +0530

    Add Blank Space [B] from Codeforces

---
+++ 1829/B - Blank Space/solution.cpp
@@ -0,0 +1,30 @@
+//  B. Blank Space
+ 
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+    int t;
+    cin>>t;
+    while (t--) {
+        int n;
+        cin>>n;
+        vector<int>arr(n);
+        for (int i=0;i<n;i++)
+            cin>>arr[i];
+        int blankspace=0;
+        int ans=0;
+        for (int i=0;i<n;i++) {
+            if (arr[i]!=0) {
+                ans=max(ans,blankspace);
+                blankspace=0;
+            }
+            else
+                blankspace++;
+        }
+        ans=max(ans,blankspace);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
