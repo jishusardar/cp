aea42947f8bfd8160e795bfa01712074fde3d3f3
Jishu Sardar <jishusardar01@gmail.com>
2026-09-17 01:31:27 +0530

    Add Bit++ [A] from Codeforces

---
+++ 282/A - Bit++/solution.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x=0;
+    int t;
+    cin>>t;
+    string s;
+    while(t){
+        cin>>s;
+        if(s=="++X"||s=="X++"){
+            x++;
+        }
+        else if(s=="X--"||s=="--X"){
+            x--;
+        }
+        t--;
+    }
+    cout<<x;
+    return 0;
+}
